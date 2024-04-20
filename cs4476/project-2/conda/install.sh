1c1021d7ac96cd3aeef307477f4f08e81fd30c05
vpokharna3 <vpokharna3@gatech.edu>
2024-04-20 19:04:25 -0400

    cs4476 proj2

---
+++ cs4476/project-2/conda/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+ENVIRONMENT_FILE=requirements.txt
+echo "Using ${ENVIRONMENT_FILE} ..."
+
+# Create library environment.
+# conda env create -f "${SCRIPT_DIR}/${ENVIRONMENT_FILE}" \
+# && eval "$(conda shell.bash hook)" \
+# && conda activate cv_proj1 \
+# && python -m pip install -e "$SCRIPT_DIR/.."
+
+conda create -n cv_proj2 python=3.10 \
+&& conda activate cv_proj2 \
+&& pip3 install -r ./requirements.txt
